b3606a0b52887e84ab54c49d5e49871806cf742d
ZLATANGORALIJA\Zlatan Goralija <zlatangoralija@yahoo.com>
2018-07-04 00:20:59 +0200

    admin master page setup

--- app/User.php
+++ app/User.php
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function role(){
+        $this->belongsTo('App\Role');
+    }
 }
